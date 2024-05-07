7527a7f8b7ad6ef77df055b895001814aad8c984
James <j.smilezz052@gmail.com>
2024-05-07 09:37:49 +0100

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const {getAllArticles, getArticle, patchAnArticle} = require('./controllers/arti
 const {getArticleComments, postAComment, deleteArticleComment} =require('./controllers/comments.controllers')
 const {getAllUsers} = require('./controllers/users.controllers')
 const endPoints = require('./endpoints.json')
+const cors = require('cors')
+
+
+app.use(cors());
 
 app.use(express.json())
 
@@ -29,12 +33,15 @@ app.delete('/api/comments/:comment_id', deleteArticleComment)
 app.get('/api/users', getAllUsers)
 
 
+
+
 app.use((err, req, res, next) => {
     if(err.code === '22P02'){
         res.status(400).send({msg: 'bad request'})
     }
     next(err)
 })
+
 app.use((err, req, res, next) => {
     if(err.status && err.msg){
         res.status(err.status).send({msg: err.msg})
